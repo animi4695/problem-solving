0cc54cf47a4d72d496e297c15eae2354741b895a
Murali k <murali.k@axiomio.com>
2020-09-22 13:17:22 +0530

    hackerrank-reverse the sentence

--- hackerrank/si-reverse-the-sentence/sol.cpp
+++ hackerrank/si-reverse-the-sentence/sol.cpp
@@ -5,18 +5,37 @@
 #include<stack>
 
 using namespace std;
-void solve(string a){
-    int p1 = 0;
-    int p2 = a.length()-1;
-    while (p1 < p2)
-    {
+string reverse(string a, int p1, int p2){
+    while (p1 < p2){
         char temp = a[p1];
         a[p1] = a[p2];
         a[p2] = temp;
         p1++;
         p2--;
     }
-    cout<<a;
+
+    return a;
+}
+void solve(string a){
+    int p1 = 0;
+    int p2 = a.length()-1;
+    // reverse complete sentence
+    a = reverse(a, p1, p2);
+
+    // reverse each word
+    p1 = 0, p2 = 0;
+    for(int i = 0; i < a.length(); i++){
+        if(a[i] != ' '){
+            p1 = i;
+            p2 = i;
+            while(i < a.length() && a[i] != ' '){
+                i++;
+            }
+            p2 = i-1;
+            a = reverse(a, p1, p2);
+        }
+    }
+    cout<<a<<"\n";
 }
 int main(){
     int t;
