13c97289ae47d32de3c096402e8182e7f21aa05d
Murali k <murali.k@axiomio.com>
2020-10-04 16:43:47 +0530

    insertion-sort-list

---
+++ interviewbit/insertion-sort-list/sol.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<climits>
+
+using namespace std;
+struct ListNode{
+    int val;
+    ListNode* next;
+    ListNode(int val){
+        val = val;
+        next = NULL;
+    }
+};
+
+ListNode* InsertionSort(ListNode* &head, ListNode* newNode){
+    ListNode* curr;
+    if(head == NULL || head->val >= newNode->val){
+        newNode->next = head;
+        head = newNode;
+    }
+    else{
+        curr = head;
+        while (curr->next != NULL && curr->next->val < newNode->val)
+            curr = curr->next;
+        
+        newNode->next = curr->next;
+        curr->next = newNode;
+    }
+}
+
+ListNode* insertionSortList(ListNode* A){
+    if(A == NULL) return A;
+    ListNode* curr = A;
+    ListNode* sorted = NULL;
+
+    while (curr != NULL)
+    {
+        ListNode* next = curr->next;
+        InsertionSort(sorted, curr);
+        curr = next;
+    }
+    A = sorted;
+    return A;
+}
+
+int main(){
+    return 0;
+}
