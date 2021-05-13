1affbca592bccb65e3e461493044aa13bd6bdbe8
Murali k <murali.k@axiomio.com>
2021-05-13 23:50:55 +0530

    Add FastIO to template

--- codechef/template.cpp
+++ codechef/template.cpp
@@ -1,9 +1,15 @@
 #include<iostream>
 #include<climits>
 #include<cmath>
+#include<vector>
+#include<algorithm>
+#include<string>
 
 using namespace std;
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int t;
     cin>>t;
     while(t--){
